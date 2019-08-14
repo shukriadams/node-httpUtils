c1bab84e728e05b40676389d53164b592c3117d7
Shukri Adams <shukri.adams@gmail.com>
2019-08-14 20:29:41 +0200

    moving in

---
+++ README.md
@@ -0,0 +1,3 @@
+# shukriadams-httputils
+
+Library of Nodejs helper functions for HTTP stuff. Not worth bloating npmjs.org for, this is for my own and client projects.
---
+++ index.js
@@ -0,0 +1,61 @@
+const 
+    fs = require('fs-extra'),
+    request = require('request'),
+    http = require('http');
+
+
+/**
+ * Downloads a file and saves it locally.
+ */    
+const downloadFile = async function (url, filePath){
+    return new Promise(async function(resolve, reject){
+        const file = fs.createWriteStream(savePath);
+
+        http.get(url, function(response) {
+            response.pipe(file);
+            file.on('finish', function() {
+                file.close(function(){
+                    resolve();
+                });  
+            });
+        }).on('error', function(error) {
+            // if download fails, delete file
+            fs.unlink(filePath);
+            reject(error);
+        });
+
+    }.bind(this));
+}
+
+
+/** 
+ * Async request. 
+ */
+const downloadString = async (options)=>{
+    return new Promise((resolve, reject)=>{
+        request( options, 
+            (error, response) =>{
+                if (error)
+                    return reject(error);
+
+                resolve(response);
+            }
+        )
+    });
+}
+
+
+/**
+ * Downloads the target url as JSON.
+ */
+const downloadJSON = async (url)=>{
+    let raw = await this.downloadString(url);
+    return JSON.parse(raw.body);
+}
+
+
+module.exports = {
+    downloadFile,
+    downloadString,
+    downloadJSON
+}
